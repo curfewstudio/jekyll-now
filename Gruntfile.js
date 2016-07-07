b393c54b9797bca8e5634d74e1fe87fd3fb69634
curfewstudio <yo@curfew.nyc>
2016-07-06 21:05:56 -0400

    updating remote url

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,7 +285,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'https://github.com/curfewstudio/curfewstudio.github.io.git',
           branch: 'gh-pages',
           commit: true,
           push: true
